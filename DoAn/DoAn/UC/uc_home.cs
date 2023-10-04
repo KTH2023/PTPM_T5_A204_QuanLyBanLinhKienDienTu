55ab1d9506fd8c0812790dfd7ca8a8369cf608c0
= <144313046+SeunYeun@users.noreply.github.com>
2023-10-04 19:30:47 +0700

    source code 2

---
+++ DoAn/DoAn/UC/uc_home.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.XtraCharts;
+using System.Data;
+using System.Globalization;
+
+namespace GUI.UC
+{
+    public partial class uc_home : DevExpress.XtraEditors.XtraUserControl
+    {
+        public uc_home()
+        {
+            InitializeComponent();
+        }
+
+        private void uc_home_Load(object sender, EventArgs e)
+        {
+            //load biểu đồ doanh thu năm hiện tại
+            loadStatisticalYear();
+            //load biểu đồ lượng nhập vào bán ra tháng hiện tại
+            loadQuantityImportAndOrder();
+            //load biểu đồ top sản phẩm bán chạy (số lượng bán >=30)
+            loadTopProductSelling();
+            //load biểu đồ các sản phẩm hết hàng
+            loadProductsNotStock();
+        }
+
+        private void loadProductsNotStock()
+        {
+            
+        }
+
+        private void loadTopProductSelling()
+        {
+            
+        }
+
+        private void loadQuantityImportAndOrder()
+        {
+            
+        }
+
+        private void loadStatisticalYear()
+        {
+            
+        }
+    }
+}
---
+++ DoAn/DoAn/UC/uc_import_employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraEditors.Controls;
+
+namespace GUI.UC
+{
+    public partial class uc_import_employee : DevExpress.XtraEditors.XtraUserControl
+    {
+        public uc_import_employee()
+        {
+            InitializeComponent();
+        }
+        private void uc_import_employee_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+    }
+}
